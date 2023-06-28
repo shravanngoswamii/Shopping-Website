774830ed4490bc48e6c0779f419194edfb0a9054
shravanngoswamii <shravanngoswamii@gmail.com>
2023-06-28 21:41:31 +0530

    adding cart.js and changes many files

--- WeMade/store/models.py
+++ WeMade/store/models.py
@@ -29,12 +29,29 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True) # on_delete=models.SET_NULL means that if the product is deleted, the order will still be there
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True) # on_delete=models.SET_NULL means that if the order is deleted, the product will still be there
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True) # auto_now_add=True means that the date will be added automatically
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+    
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True) # on_delete=models.SET_NULL means that if the customer is deleted, the shipping address will still be there
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True) # on_delete=models.SET_NULL means that if the order is deleted, the shipping address will still be there
